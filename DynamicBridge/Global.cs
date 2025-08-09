8716e04f8f970c1aace4a0bf7d284046ccdc4b85
Limiana <5073202+Limiana@users.noreply.github.com>
2025-08-09 09:26:21 +0300

    7.3

--- DynamicBridge/Global.cs
+++ DynamicBridge/Global.cs
@@ -10,7 +10,7 @@ global using ECommons.DalamudServices.Legacy;
 global using ECommons.ImGuiMethods;
 global using ECommons.Logging;
 global using ECommons.Schedulers;
-global using ImGuiNET;
+global using Dalamud.Bindings.ImGui;
 global using System;
 global using System.Collections.Generic;
 global using System.Linq;
